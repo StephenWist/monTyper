0cb3b589b95aa2b3a26b47a7633e4e2d827e5d82
Stephen Wist <stphnwist@gmail.com>
2024-03-02 11:00:37 -0500

    Initial type colors

--- src/Card.svelte
+++ src/Card.svelte
@@ -4,6 +4,12 @@
     export { className as class };
     export let eff;
     export let type;
+
+    let type_colors = {'Normal':'#929DA3',
+                        'Fighting': '#CE416B',
+                        'Flying': '#8FA9DE',
+                        'Ground':'#E19F87',
+    }
 </script>
 
 <style>
@@ -14,7 +20,8 @@
         margin: 5px;
         width:140px;
         height: 70px;
-        background-color: rgb(226, 236, 225);
+        border: 2px solid black;
+        background-color: #ffffff;
         border-radius: 5px;
     }
 
@@ -22,9 +29,28 @@
         margin: 5px;
         font-size: 1.1rem;
     }
+
+    .Normal {background-color: #929DA3;}
+    .Fighting {background-color: #CE416B;}
+    .Flying {background-color: #8FA9DE;}
+    .Ground {background-color: #E19F87;}
+    .Poison {background-color: #BF99D4;}
+    .Rock {background-color: #d3c8aa;}
+    .Bug { background-color: #add080;}
+    .Ghost { background-color: #8e97c0;}
+    .Steel { background-color: #90acb8;}
+    .Fire { background-color: #ffb68d;}
+    .Water { background-color: #8cade0;}
+    .Grass { background-color: #94cb91;}
+    .Electric { background-color: #f6dd83;}
+    .Psychic { background-color: #fa9c9d;}
+    .Ice { background-color: #9cdacd;}
+    .Dragon { background-color: #7a99cf;}
+    .Dark { background-color: #908d96;}
+    .Fairy { background-color: #f2adec;}
 </style>
 
-<div class={className}>
+<div class='{className} {type}'>
     <TypeImage src='../{type}.png' />
     {#if eff != 0}
         <p>{eff}X Effective</p>
