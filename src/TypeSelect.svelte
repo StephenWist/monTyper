d12fee9a1de8fd03ec4a2ea71c32137352687930
Stephen Wist <stphnwist@gmail.com>
2024-03-05 15:50:16 -0500

    Subscribe TypeSelect to gen.

--- src/TypeSelect.svelte
+++ src/TypeSelect.svelte
@@ -46,7 +46,7 @@
     {/if}
 </label>
 <select bind:value={selected} on:change={typeUpdate} id={id}>
-{#each types['1'] as type}
+{#each types[$gen] as type}
         <option>{type}</option>
     {/each}
 </select>
