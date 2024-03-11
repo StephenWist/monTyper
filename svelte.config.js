3ece75fdc7dde51225448af614152277b0228675
Stephen Wist <stphnwist@gmail.com>
2024-03-11 18:38:44 -0400

    Touch some scss files and add rollup.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
+import svelte from 'rollup-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
